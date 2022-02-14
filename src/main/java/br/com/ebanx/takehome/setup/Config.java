bfbfc730f9b164fa6a2e9f5df137a9812503d861
Odione <odione.soethe@sensedia.com>
2022-02-14 14:31:04 -0300

    removido config

--- src/main/java/br/com/ebanx/takehome/setup/Config.java
+++ src/main/java/br/com/ebanx/takehome/setup/Config.java
@@ -1,10 +1,6 @@
 package br.com.ebanx.takehome.setup;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.util.MinimalPrettyPrinter;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,23 +11,6 @@ public class Config {
   @Bean
   Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
     return builder -> builder
-        .serializationInclusion(Include.NON_NULL)
-        .indentOutput(true)
-        .postConfigurer(this::configurePrinter);
-  }
-
-  void configurePrinter(ObjectMapper objectMapper) {
-    var printer = new MinimalPrettyPrinter() {
-      @Override
-      public void writeStartObject(JsonGenerator g) throws IOException {
-        g.writeRaw(" {");
-      }
-
-      @Override
-      public void writeObjectEntrySeparator(JsonGenerator g) throws IOException {
-        g.writeRaw(", ");
-      }
-    };
-    objectMapper.setDefaultPrettyPrinter(printer);
+        .serializationInclusion(Include.NON_NULL);
   }
 }
